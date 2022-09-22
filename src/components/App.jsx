3f1e8e120195b5fa10d6794582df661e31bb7f3d
Andrew <93881817+Bachynckyi@users.noreply.github.com>
2022-09-23 00:19:43 +0300

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,38 +10,43 @@ export class App extends Component {
     page: "1"
   }
 
-  addMaterial = async (query, page) =>  {
+  async componentDidUpdate(prevProps, prevState) {
+    if(prevState.query !== this.state.query)
     try {    
-      this.setState({isLoading: true});
-      const data = await API.fetchData(query, page);
-      this.setState(state => ({dataImages: [...state.dataImages, ...data.hits], isLoading:false}));
-      console.log(data);
-    }
-    catch(error) {
-      console.log(error)
-    }
-  };
+        this.setState({isLoading: true});
+        const data = await API.fetchData(this.state.query, this.state.page);
+        setTimeout(() => {
+          this.setState(state => ({dataImages: [...state.dataImages, ...data.hits], isLoading:false}));
+        }, 1000);
+
+      }
+      catch(error) {
+        console.log(error)
+      }
+  }
 
   handleformSubmit = newQuery => {
-    this.setState({query: newQuery});
+    this.setState({query: newQuery, dataImages: [], page: 1});
+    window.scrollTo({ top: 0, left: 0 });
   }
 
   render() {
     const { dataImages } = this.state;
     return (
-      <div
+      <div 
         style={{
           display: 'flex',
           flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
           fontSize: 40,
-          color: '#010101'
+          color: '#010101',
+          paddingBottom: 24,
         }}
       >
         <SearchBar onSubmit={this.handleformSubmit}/>
         {this.state.isLoading && <h1>Загрузочка</h1>}
-        {dataImages.length > 0 ? (<ImageGallery dataImages={dataImages} />) : (<span>nothing</span>)}
+        {dataImages.length > 0 && (<ImageGallery dataImages={dataImages} />)}
       </div>
     );
   }
--- src/services/API.js
+++ src/services/API.js
@@ -1,10 +1,17 @@
 import axios from "axios";
 
-const BASEURL = 'https://pixabay.com/api/'
-const KEY = '28946050-edbdf2e2dca91bf63c1cc8e01';
-const URL = `${BASEURL}?q=cat&page=1&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`;
-
 export const fetchData = async (query, page) => {
-    const response = await axios.get(URL)
-    return response.data;
-}
+    const BASEURL = 'https://pixabay.com/api/'
+    const KEY = '28946050-edbdf2e2dca91bf63c1cc8e01';
+    const URL = `${BASEURL}?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`;
+
+    try {
+        const response = await axios.get(URL)
+        return response.data;
+    }
+    catch(error) {
+        console.log(error)
+    }
+};
+
+export default fetchData;
